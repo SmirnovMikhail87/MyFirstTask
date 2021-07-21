eca9b18019e08865290568418376372f6e9bddf1
Mikhail Smirnov <smirnov.ma.1987@gmail.com>
2021-07-21 11:47:54 +0300

    Потренировался с обработкой кнопки "Привет"

--- app/src/main/java/com/smirnovma/myfirsttask/MainActivity.java
+++ app/src/main/java/com/smirnovma/myfirsttask/MainActivity.java
@@ -3,12 +3,22 @@ package com.smirnovma.myfirsttask;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private TextView textView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        textView = findViewById(R.id.text);
+    }
+
+    public void sayHello(View view){
+        textView.setText("Привет Андрей)");
     }
 }
